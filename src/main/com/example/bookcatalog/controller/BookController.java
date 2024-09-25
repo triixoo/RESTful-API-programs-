ee72b92ac48f9491304eba0e6410ca0802630844
triixoo <karaulsyngys89@gmail.com>
2024-09-25 23:32:21 +0500

    Задача данная компанией

--- src/main/com/example/bookcatalog/controller/BookController.java
+++ src/main/com/example/bookcatalog/controller/BookController.java
@@ -7,9 +7,49 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 public class BookController {
-
+    @RestController
+    @RequestMapping("/books")
     private final BookServise bookServise;
 
-    public 
+    public BookController(BookService bookService){
+        this.bookServise = bookService;
+    }
+    @GetMapping
+    public List<Book> getAllBooks(){
+        return bookServise.getAllBooks();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookID(@PathVariable Long id){
+        Book book = bookServise.getbookID(id);
+        if (book == null) {
+            return ResponseEntity.ok(book);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+    @PostMapping
+    public ResponseEntity<Book> addBook(@RequestBody Book book) {
+        Book newBook = bookService.addBook(book);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newBook);
+    }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book book) {
+        Book updatedBook = bookService.updateBook(id, book);
+        if (updatedBook != null) {
+            return ResponseEntity.ok(updatedBook);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
+        boolean isRemoved = bookService.deleteBook(id);
+        if (isRemoved) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
 }
