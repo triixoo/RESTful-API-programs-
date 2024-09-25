2ce87c6857e7d9d7dd94ba7a8365f9b62b8451fa
triixoo <karaulsyngys89@gmail.com>
2024-09-25 23:14:52 +0500

    Задача данная компанией

---
+++ src/main/com/example/bookcatalog/controller/BookController.java
@@ -0,0 +1,15 @@
+package com.example.bookcatalog.controller;
+
+import com.example.bookcatalog.model.Book;
+import com.example.bookcatalog.service.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+public class BookController {
+
+    private final BookServise bookServise;
+
+    public 
+
+}
---
+++ src/main/com/example/bookcatalog/model/Book.java
@@ -0,0 +1,9 @@
+package com.example.bookcatalog.model;
+
+public class Book {
+    private Long id;
+    private String title;
+    private String author;
+    private String description;
+    private Double price;
+}
